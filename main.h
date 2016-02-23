5367c2b16ccbbe2421d82d2cfb803d9641735776
skollhati <rlacksdyd@hanmail.net>
2016-02-23 17:37:43 +0900

    first upload

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+#include<Windows.h>
+#include<process.h>
+#include<CommCtrl.h>
+#include<stdlib.h>
+#include<mmsystem.h>
+#include<time.h>
+#include<tchar.h>
+
+#include<vector>
+#include<conio.h>
+
+#endif
